8197df0ea923b2adfb884d055918e335d9071af5
laboratorio <laboratorio@LAB11-22.ulatina.net>
2017-09-25 19:01:27 -0600

    Initial commit

---
+++ src/container/List.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package container;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class List {
+    private int size;
+    private Node head;
+
+    public List() {
+        this.size = 0;
+        this.head = null;
+    }
+
+    public int size() {
+        return size;
+    }
+    
+    
+}
---
+++ src/container/Node.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package container;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Node {
+    private String data;
+    private Node next;
+    private Node prev;
+
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+    
+    
+    
+}
